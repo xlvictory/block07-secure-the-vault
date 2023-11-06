3bd03b30e3505fca9f202c6529c491d651624acb
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2023-11-06 14:21:56 -0500

    Added psuedocode

---
+++ secure-the-vault.js
@@ -0,0 +1,8 @@
+/*
+1. Create a html file to go with the javascript file 
+2. Link aforementioned .js file in the HTML head tag
+3. Declare variables (combo1, combo2, combo3)
+4. Use arithmetic operators when declaring the value of each variable
+5. Run each variable, just to ensure that they amount to the value they're supposed to (personal choice)
+6. Use the alert() to compose the string plus added variables
+*/
