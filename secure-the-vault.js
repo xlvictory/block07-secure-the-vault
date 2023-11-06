edcaebd36ad8c7f3b1424dc89c5eb0b93828a5d5
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2023-11-06 14:27:40 -0500

    Declared variables and tested arithmetic

--- secure-the-vault.js
+++ secure-the-vault.js
@@ -6,3 +6,13 @@
 5. Run each variable, just to ensure that they amount to the value they're supposed to (personal choice)
 6. Use the alert() to compose the string plus added variables
 */
+
+// declaring variables
+let combo1 = (5 * 4) / 2
+let combo2 = 15 + 25
+let combo3 = (6 * 5) + (3*3)
+
+//checking that my arithmetic works!!
+console.log(combo1)
+console.log(combo2)
+console.log(combo3)
