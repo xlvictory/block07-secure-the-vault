bd95894e64d06993a1c40be8749f1b1f19acd69a
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2023-11-06 14:43:37 -0500

    Created string variable and utilized alert() in two different ways

--- secure-the-vault.js
+++ secure-the-vault.js
@@ -4,15 +4,25 @@
 3. Declare variables (combo1, combo2, combo3)
 4. Use arithmetic operators when declaring the value of each variable
 5. Run each variable, just to ensure that they amount to the value they're supposed to (personal choice)
-6. Use the alert() to compose the string plus added variables
+6. Create a variable for the string
+7. Create alert() using variables
 */
 
 // declaring variables
-let combo1 = (5 * 4) / 2
-let combo2 = 15 + 25
-let combo3 = (6 * 5) + (3*3)
+let combo1 = (5 * 4) / 2;
+let combo2 = 15 + 25;
+let combo3 = (6 * 5) + (3*3);
 
-//checking that my arithmetic works!!
+/* checking that my arithmetic works!!
 console.log(combo1)
 console.log(combo2)
-console.log(combo3)
+console.log(combo3) */
+
+//creating string variable
+const alertStr = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+//Using alert()
+alert(`${alertStr}${combo1}-${combo2}-${combo3}.`)
+
+/* other way of coding the alert()
+alert(alertStr + combo1 + "-" + combo2 + "-" + combo3)*/
